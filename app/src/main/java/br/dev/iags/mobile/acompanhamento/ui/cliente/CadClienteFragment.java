c8278e0d5cbc3eb57cb380000b452795779004d4
icaroalvesg <icarogomessantos2@gmail.com>
2023-08-21 17:10:57 -0300

    10-08-2023 Metodos jason ok

--- app/src/main/java/br/dev/iags/mobile/acompanhamento/ui/cliente/CadClienteFragment.java
+++ app/src/main/java/br/dev/iags/mobile/acompanhamento/ui/cliente/CadClienteFragment.java
@@ -1,5 +1,6 @@
 package br.dev.iags.mobile.acompanhamento.ui.cliente;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -9,20 +10,65 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import br.dev.iags.mobile.acompanhamento.R;
+import br.dev.iags.mobile.acompanhamento.model.Pedido;
 
 
-public class CadClienteFragment extends Fragment {
+public class CadClienteFragment extends Fragment implements View.OnClickListener {
 
     private View view = null;
+    private Spinner spServico;
+    private EditText etCPF;
+    private EditText etDetalhe;
+    private CalendarView cvData;
+    private Button btSalvar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         this.view = inflater.inflate(R.layout.fragment_cad_cliente, container, false);
+        //
+        this.spServico = (Spinner) view.findViewById(R.id.spServico);
+        this.etCPF = (EditText) view.findViewById(R.id.etCPF);
+        this.etDetalhe = (EditText) view.findViewById(R.id.etDetalhe);
+        this.cvData = (CalendarView)view.findViewById(R.id.cvData);
+        this.btSalvar = (Button) view.findViewById(R.id.btSalvar);
+        this.btSalvar.setOnClickListener(this);
+        //
         return this.view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.btSalvar:
+                Pedido pedido = new Pedido();
+                pedido.setCodServico(this.spServico.getSelectedItemPosition());
+                pedido.setCpfCliente(this.etCPF.getText().toString());
+                pedido.setDetalheServico(this.etDetalhe.getText().toString());
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                String dataSelecionada = sdf.format(new Date(cvData.getDate()));
+                pedido.setData(dataSelecionada);
+
+                //mensagem de sucesso
+                Context context = view.getContext();
+                CharSequence text = "salvo com sucesso!";
+                int duration = Toast.LENGTH_SHORT;
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+                break;
+        }
+
+    }
 }
